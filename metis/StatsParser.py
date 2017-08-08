776af9ad7056af5834d7fde3f34abda536ec3824
Nick Amin <amin.nj@gmail.com>
2017-08-07 20:37:14 -0500

    :no_entry_sign: remove unused class

--- metis/StatsParser.py
+++ metis/StatsParser.py
@@ -7,49 +7,6 @@ from pprint import pprint
 import LogParser
 import Utils
 
-class CustomEncoder(json.JSONEncoder):
-    """
-    stolen from
-    https://stackoverflow.com/questions/16264515/json-dumps-custom-formatting
-    """
-    def __init__(self, *args, **kwargs):
-        super(CustomEncoder, self).__init__(*args, **kwargs)
-        self.current_indent = 0
-        self.current_indent_str = ""
-
-    def encode(self, o):
-        #Special Processing for lists
-        if isinstance(o, (list, tuple)):
-            primitives_only = True
-            for item in o:
-                if isinstance(item, (list, tuple, dict)):
-                    primitives_only = False
-                    break
-            output = []
-            if primitives_only:
-                for item in o:
-                    output.append(json.dumps(item))
-                return "[ " + ", ".join(output) + " ]"
-            else:
-                self.current_indent += self.indent
-                self.current_indent_str = "".join( [ " " for x in range(self.current_indent) ])
-                for item in o:
-                    output.append(self.current_indent_str + self.encode(item))
-                self.current_indent -= self.indent
-                self.current_indent_str = "".join( [ " " for x in range(self.current_indent) ])
-                return "[\n" + ",\n".join(output) + "\n" + self.current_indent_str + "]"
-        elif isinstance(o, dict):
-            output = []
-            self.current_indent += self.indent
-            self.current_indent_str = "".join( [ " " for x in range(self.current_indent) ])
-            for key, value in o.iteritems():
-                output.append(self.current_indent_str + json.dumps(key) + ": " + self.encode(value))
-            self.current_indent -= self.indent
-            self.current_indent_str = "".join( [ " " for x in range(self.current_indent) ])
-            return "{\n" + ",\n".join(output) + "\n" + self.current_indent_str + "}"
-        else:
-            return json.dumps(o)
-
 def merge_histories(hold, hnew):
     if not hold: return hnew
     for key in hnew.keys():
