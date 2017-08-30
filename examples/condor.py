29cc00f804d3cbbe889d8e119f16813ac319c0fb
Nick Amin <amin.nj@gmail.com>
2017-08-30 10:41:28 -0700

    :nut_and_bolt: minor improvements to printouts

--- examples/condor.py
+++ examples/condor.py
@@ -14,15 +14,20 @@ if __name__ == "__main__":
     # Do condor_q and print out a dict for the first job
     # By default, only shows jobs for $USER
     my_jobs = condor_q()
-    pprint(my_jobs[0])
-    print
+    if my_jobs:
+        print "-- my jobs --"
+        pprint(my_jobs[0])
+        print 
 
     # Don't specify a user, so get all jobs
     all_jobs = condor_q(user="")
-    pprint(all_jobs[0])
-    print
+    if all_jobs:
+        print "-- all jobs --"
+        pprint(all_jobs[0])
+        print
 
     # Get all job statuses and print out a counts of the
     # different statuses
     all_statuses = [job["JobStatus"] for job in all_jobs]
-    print get_hist(all_statuses)
+    if all_statuses:
+        print get_hist(all_statuses)
