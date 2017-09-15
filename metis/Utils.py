dad7a8822b06cfb593f90f00bc09344b89bc373c
Nick Amin <amin.nj@gmail.com>
2017-09-15 07:16:33 -0700

    :anguished: dont be so greedy

--- metis/Utils.py
+++ metis/Utils.py
@@ -204,8 +204,8 @@ def condor_submit(**kwargs): # pragma: no cover
 
     if kwargs.get("use_xrootd", False): params["sites"] = kwargs.get("sites","T2_US_UCSD,T2_US_Wisconsin,T2_US_Florida,T2_US_Purdue,T2_US_Nebraska,T2_US_Caltech")
     else: params["sites"] = kwargs.get("sites","T2_US_UCSD")
-    if os.getenv("USER") in ["namin"] and "T2_US_UCSD" in params["sites"]:
-        params["sites"] += ",UAF,UCSB"
+    # if os.getenv("USER") in ["namin"] and "T2_US_UCSD" in params["sites"]:
+    #     params["sites"] += ",UAF,UCSB"
 
     params["extra"] = ""
     if "selection_pairs" in kwargs:
