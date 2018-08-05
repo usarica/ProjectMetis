83fdb5a538e9111610543d0ed8a5a11aeb50a995
Nick Amin <amin.nj@gmail.com>
2018-08-05 15:23:01 -0700

    add local merging task and make mtest prettier

---
+++ metis/LocalMergeTask.py
@@ -0,0 +1,69 @@
+import os
+
+from metis.Task import Task
+from metis.File import File, MutableFile
+import metis.Utils as Utils
+
+import ROOT as r
+
+class LocalMergeTask(Task):
+    def __init__(self, **kwargs):
+        """
+        Takes a list of input file paths and a single full absolute output filename
+        and performs a single merge operation
+        """
+        self.input_filenames = kwargs.get("input_filenames", [])
+        self.output_filename = kwargs.get("output_filename", [])
+        self.io_mapping = kwargs.get("io_mapping", [])
+        self.update_mapping()
+        super(self.__class__, self).__init__(**kwargs)
+
+    def get_inputs(self):
+        return sum([x[0] for x in self.io_mapping], [])
+
+    def get_outputs(self):
+        return sum([x[1] for x in self.io_mapping], [])
+
+    def update_mapping(self):
+        if self.io_mapping: return
+        self.io_mapping = [ 
+                [
+                    map(File,self.input_filenames),
+                    [File(self.output_filename),]
+                    ] 
+                ]
+
+    def process(self):
+        done = all(map(lambda x: x.exists(), self.get_outputs()))
+        self.logger.info("Begin processing")
+        if not done:
+            self.merge_function(self.get_inputs(), self.get_outputs()[0])
+            pass
+        self.logger.info("End processing")
+
+    def merge_function(self, inputs, output):
+        # make the directory hosting the output if it doesn't exist
+        fdir = output.get_basepath()
+        if not os.path.exists(fdir): Utils.do_cmd("mkdir -p {0}".format(fdir))
+
+        fm = r.TFileMerger(True)
+        fm.OutputFile(output.get_name())
+        fm.SetFastMethod(True)
+        ngood = 0
+        for inp in inputs:
+            ngood += fm.AddFile(inp.get_name(), False)
+        if ngood != len(inputs):
+            raise RuntimeError("Trying to merge {0} files into {1}, but only {2} of them got included properly".format(len(inputs), output.get_name(), ngood))
+        self.logger.info("Added {0} files to be merged".format(len(inputs)))
+        fm.Merge()
+        self.logger.info("Done merging files into {0}".format(output.get_name()))
+
+        # ch = r.TChain("t")
+        # for inp in inputs:
+        #     ch.Add(inp.get_name())
+        # self.logger.info("Added {0} files to be merged, containing {1} events".format(len(inputs),ch.GetEntries()))
+        # ch.Merge(output.get_name(), "fast")
+        # self.logger.info("Done merging files into {0}".format(output.get_name()))
+
+if __name__ == "__main__":
+    pass
