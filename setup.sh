0a07223553e4d878b51e5e4ad0d808a299f0d008
Nick Amin <amin.nj@gmail.com>
2021-01-08 15:21:45 -0800

    :beetle: true/false didn't matter, only the emptiness; make that clear

--- setup.sh
+++ setup.sh
@@ -16,7 +16,7 @@ export PYTHONPATH=${METIS_BASE}:$PYTHONPATH
 # Add some scripts to the path
 export PATH=${METIS_BASE}/scripts:$PATH
 
-export USEDASGOCLIENT=true
+export USEDASGOCLIENT=x # or USEDASGOCLIENT= to fall back to dis
 
 # export GRIDUSER=$(voms-proxy-info -identity -dont-verify-ac | cut -d '/' -f6 | cut -d '=' -f2)
 export GRIDUSER=$(voms-proxy-info -identity | cut -d '/' -f6 | cut -d '=' -f2)
